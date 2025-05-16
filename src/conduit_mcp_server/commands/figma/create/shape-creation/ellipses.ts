6a323d45498d7a36f754734607211b5a225a7279
eonist <30n1st@gmail.com>
2025-05-16 21:12:19 +0200

    Update ellipses.ts

--- src/conduit_mcp_server/commands/figma/create/shape-creation/ellipses.ts
+++ src/conduit_mcp_server/commands/figma/create/shape-creation/ellipses.ts
@@ -5,7 +5,7 @@ import { EllipseSchema } from "./ellipse-schema.js";
 import { processBatch } from "../../../../utils/batch-processor.js";
 import { v4 as uuidv4 } from "uuid";
 import { handleToolError } from "../../../../utils/error-handling.js";
-import { isValidNodeId } from "../../../utils/figma/is-valid-node-id.js";
+import { isValidNodeId } from "../../../../utils/figma/is-valid-node-id.js";
 
 /**
  * Registers ellipse creation commands with the MCP server.
