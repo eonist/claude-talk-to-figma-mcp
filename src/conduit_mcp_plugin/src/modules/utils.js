c7dfc066d605f97108137f8b832dfbe68d3ed208
eonist <30n1st@gmail.com>
2025-05-19 01:42:58 +0200

    Update utils.js

--- src/conduit_mcp_plugin/src/modules/utils.js
+++ src/conduit_mcp_plugin/src/modules/utils.js
@@ -1,4 +1,51 @@
 /**
- * This file is intentionally left empty.
- * All utilities are included directly in the build via moduleOrder in build.js.
+ * Utils operations module.
+ * Aggregates and re-exports all utility functions and state for the Conduit MCP Figma plugin.
+ * Follows the same pattern as styles.js for consistency.
+ *
+ * Exposed functions:
+ * - pluginCoreState
+ * - sendProgressUpdate
+ * - initializePlugin
+ * - updateSettings
+ * - delay
+ * - generateCommandId
+ * - uniqBy
+ * - setCharacters
+ * - canAcceptChildren
+ * - customBase64Encode
+ *
+ * @module modules/utils
  */
+
+import { pluginCoreState, sendProgressUpdate, initializePlugin, updateSettings } from "./utils/plugin.js";
+import { delay, generateCommandId, uniqBy, setCharacters, canAcceptChildren } from "./utils/helpers.js";
+import { customBase64Encode } from "./utils/encoding.js";
+
+// Export all utilities individually
+export {
+  pluginCoreState,
+  sendProgressUpdate,
+  initializePlugin,
+  updateSettings,
+  delay,
+  generateCommandId,
+  uniqBy,
+  setCharacters,
+  canAcceptChildren,
+  customBase64Encode
+};
+
+// Namespace object for all utils
+export const utilsOperations = {
+  pluginCoreState,
+  sendProgressUpdate,
+  initializePlugin,
+  updateSettings,
+  delay,
+  generateCommandId,
+  uniqBy,
+  setCharacters,
+  canAcceptChildren,
+  customBase64Encode
+};
