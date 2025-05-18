35a073ba68f7465c3d9c24db720cc669062e90f2
eonist <30n1st@gmail.com>
2025-05-19 01:42:27 +0200

    Update utils.js

--- src/conduit_mcp_plugin/src/modules/utils.js
+++ src/conduit_mcp_plugin/src/modules/utils.js
@@ -1,9 +1,4 @@
 /**
- * Aggregator for all utility modules.
- * Re-exports all plugin state, helpers, and encoding utilities.
- * No logic is defined here.
+ * This file is intentionally left empty.
+ * All utilities are included directly in the build via moduleOrder in build.js.
  */
-
-export * from "./utils/plugin.js";
-export * from "./utils/helpers.js";
-export * from "./utils/encoding.js";
