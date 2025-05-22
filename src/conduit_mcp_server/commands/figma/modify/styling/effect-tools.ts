fd58c41b75a622cab3ee5d8833c1ccce740aa793
eonist <30n1st@gmail.com>
2025-05-22 06:46:59 +0200

    Fix schema pt2

--- src/conduit_mcp_server/commands/figma/modify/styling/effect-tools.ts
+++ src/conduit_mcp_server/commands/figma/modify/styling/effect-tools.ts
@@ -3,7 +3,7 @@ import { FigmaClient } from "../../../../clients/figma-client.js";
 import { z } from "zod";
 import { ensureNodeIdIsString } from "../../../../utils/node-utils.js";
 import { isValidNodeId } from "../../../../utils/figma/is-valid-node-id.js";
-import { EffectSchema } from "../property-manipulation/effect-schema.js";
+import { EffectSchema } from "../property-manipulation/schema/effect-schema.js";
 import { MCP_COMMANDS } from "../../../../types/commands.js";
 
 /**
