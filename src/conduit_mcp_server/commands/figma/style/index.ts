3c16eee36479516698fac94e205da3d2c5cde7a6
eonist <30n1st@gmail.com>
2025-05-25 13:13:19 +0200

    Update index.ts

--- src/conduit_mcp_server/commands/figma/style/index.ts
+++ src/conduit_mcp_server/commands/figma/style/index.ts
@@ -1,7 +1,7 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { FigmaClient } from "../../../clients/figma-client/index.js";
 
-import { registerStyleTools } from "./style-tools.js";
+import { registerStyleTools, registerNodeStylesTool } from "./style-tools.js";
 import { registerTextStyleTools } from "./text-style-tools.js";
 import { registerFillTools } from "./fill-tools.js";
 import { registerGradientTools } from "./gradient-tools.js";
@@ -15,6 +15,7 @@ export function registerStyleCommands(server: McpServer, figmaClient: FigmaClien
   registerEffectTools(server, figmaClient);
   registerEffectsTools(server, figmaClient);
   registerStyleTools(server, figmaClient); // fixme add comment
+  registerNodeStylesTool(server, figmaClient); // Register get_node_styles command
   registerTextStyleTools(server, figmaClient); // fixme: add doc
   registerFontTools(server, figmaClient);
 }
