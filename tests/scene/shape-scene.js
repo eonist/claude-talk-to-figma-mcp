527f3c71c57044de798a58554ab73a87a6e83a0b
eonist <30n1st@gmail.com>
2025-05-25 03:58:10 +0200

    Update shape-scene.js

--- tests/scene/shape-scene.js
+++ tests/scene/shape-scene.js
@@ -1,5 +1,5 @@
-import { ws, channel, runStep } from "../test-runner.js";
 import { randomColor } from "../helper.js";
+import { channel, runStep, ws } from "../test-runner.js";
 
 /**
  * Helper to create a rectangle in Figma for shape tests.
@@ -121,7 +121,7 @@ function create_heart() {
     width: 100, height: 90,
     name: 'UnitTestHeart',
     vectorPaths: [
-      { windingRule: "NONE", data: "M50 15 C35 0 0 0 0 37.5 C0 75 50 90 50 90 C50 90 100 75 100 37.5 C100 0 65 0 50 15 Z" }
+      { windingRule: "NONE", data: "M 50 15 C 35 0 0 0 0 37.5 C 0 75 50 90 50 90 C 50 90 100 75 100 37.5 C 100 0 65 0 50 15 Z" }
     ],
     fillColor: randomColor(),
     strokeColor: randomColor(),
@@ -142,10 +142,10 @@ function create_heart() {
  * @returns {Promise<void>}
  */
 export async function shapeScene(results) {
-  results.push(await create_rectangle());
-  results.push(await create_ellipse());
-  results.push(await create_frame());
-  results.push(await create_hexagon());
-  results.push(await create_star());
+  // results.push(await create_rectangle());
+  // results.push(await create_ellipse());
+  // results.push(await create_frame());
+  // results.push(await create_hexagon());
+  // results.push(await create_star());
   results.push(await create_heart());
 }
