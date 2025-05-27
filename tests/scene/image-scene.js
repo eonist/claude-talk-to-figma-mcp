b7b54e7c8c6823380cc75fb64fb46c05e045d7ae
eonist <30n1st@gmail.com>
2025-05-27 10:25:25 +0200

    Create image-scene.js

---
+++ tests/scene/image-scene.js
@@ -0,0 +1,19 @@
+// can we make an image-scene.js based on svg-scene.js in structure
+
+// create 1 frame with autolayout, with wrapping padding gaps etc just like in svg-scene.js
+
+// download the images
+
+// resize them similar to how we resize svgs. maintain ratio, fit inside 50x50 
+
+// we need to use similar fetch code for the base64 as we used in svg-scene as figma plugin doesnt allow urls it will throw an error because of sandbozing etc
+
+// please add create_img1, 2 3 etc. in the main funciton of the file so we can easily debug etc. 
+
+// and pass frame id just like we do scg-scene.js etc
+
+// Cat: https://i.imgur.com/CzXTtJV.jpg
+// 
+// Bird: https://farm4.staticflickr.com/3075/3168662394_7d7103de7d_z_d.jpg
+// 
+// Fox: https://farm4.staticflickr.com/3852/14447103450_2d0ff8802b_z_d.jpg
